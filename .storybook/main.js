cf5cd79e022465badc729db8c3773a4708608bcd
Rubén <ruben@swingby.network>
2020-10-27 11:57:43 +0100

    chore: Readable class names inside Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,6 +7,10 @@ module.exports = {
     { name: '@storybook/addon-essentials', options: { backgrounds: false } },
   ],
   typescript: { check: true },
+  babel: (options) => {
+    options.plugins.push(require.resolve('babel-plugin-styled-components'));
+    return options;
+  },
   webpackFinal: async (config) => {
     // Make sure SVGs are not loaded with `file-loader`.
     config.module.rules = config.module.rules.map((it) => ({
