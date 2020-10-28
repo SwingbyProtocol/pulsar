24fc3a45017d9d6637dfa89589faa6e1a1efad7a
Rubén <ruben@swingby.network>
2020-10-28 13:46:01 +0100

    feat: Export `PulsarConstants`

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-export { pulsarDarkColors, pulsarLightColors } from './modules/themes';
+export { PulsarConstants } from './modules/themes';
 export { PulsarGlobalStyles } from './modules/global';
 
 export { Button } from './components/Button';
--- src/modules/themes/index.tsx
+++ src/modules/themes/index.tsx
@@ -1,2 +1,3 @@
 export { pulsarLightColors, pulsarDarkColors } from './colors';
 export { PulsarGlobalAutoTheming } from './PulsarGlobalAutoTheming';
+export { PulsarConstants } from './constants';
