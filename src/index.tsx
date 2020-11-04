87f493f9b99d84e82309b28b4c206b89074ba506
tarrio <73276750+tarrio@users.noreply.github.com>
2020-11-04 16:19:56 +0100

    feat: Added coin icons (#17)

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 export { PulsarTheme, PulsarThemeProvider, PulsarThemeType } from './modules/themes';
 export { PulsarGlobalStyles } from './modules/global';
+export { logos } from './modules/logos';
 
 export { Button } from './components/Button';
 export { Dropdown } from './components/Dropdown';
 export { Icon } from './components/Icon';
 export { ListItem } from './components/ListItem';
-export { Logo } from './components/Logo';
 export { TextInput } from './components/TextInput';
 export { Tooltip } from './components/Tooltip';
