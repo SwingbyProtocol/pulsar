7164d425786c93bbf90d7abd46746f6ebddab8a2
Rubén <ruben@swingby.network>
2020-11-06 09:20:57 +0100

    fix(AppLogo): Add `className` prop

--- src/components/AppLogo/index.tsx
+++ src/components/AppLogo/index.tsx
@@ -5,12 +5,13 @@ import { Icon } from '../Icon';
 
 import { AppLogoContainer, AppLogoText } from './styles';
 
-type Props = Testable & { productName?: string; onClick?: () => void };
+type Props = Testable & { productName?: string; onClick?: () => void; className?: string };
 
-export const AppLogo = ({ onClick, 'data-testid': testId, productName }: Props) => {
+export const AppLogo = ({ onClick, 'data-testid': testId, productName, className }: Props) => {
   const { buildTestId } = useBuildTestId({ id: testId });
   return (
     <AppLogoContainer
+      className={className}
       data-testid={buildTestId('')}
       onClick={onClick}
       as={onClick ? 'button' : 'div'}
