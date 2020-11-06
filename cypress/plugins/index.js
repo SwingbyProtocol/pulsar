3397e96b79fa8467d0e048903fbd69cb965d0486
Rubén <ruben@swingby.network>
2020-11-06 10:49:49 +0100

    chore: Upgraded dependencies

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,4 +1,4 @@
-const percyHealthCheck = require('@percy/cypress/task');
+const percyHealthCheck = require('@percy/cypress/task'); // eslint-disable-line import/no-internal-modules
 
 module.exports = (on, config) => {
   on('task', percyHealthCheck);
