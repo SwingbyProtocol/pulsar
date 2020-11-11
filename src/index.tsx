d46272668f8d8ce89df518df7a82b79c0f87c3f4
Rubén <ruben@swingby.network>
2020-11-11 12:09:35 +0100

    feat: Exported `reset`

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ export { PulsarGlobalStyles } from './modules/global';
 export { logos } from './modules/logos';
 export { formatCryptoAsset, formatFiatAsset } from './modules/intl';
 export { useMatchMedia } from './modules/match-media';
+export { reset } from './modules/reset';
 
 export { AppLogo } from './components/AppLogo';
 export { Button } from './components/Button';
