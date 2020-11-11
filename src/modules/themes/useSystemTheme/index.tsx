adf867bd324ef821f8752a7baaff6df96a85d854
Rubén <ruben@swingby.network>
2020-11-11 10:47:14 +0100

    feat: `useMatchMedia`

--- src/modules/themes/useSystemTheme/index.tsx
+++ src/modules/themes/useSystemTheme/index.tsx
@@ -1,46 +1,12 @@
-import { useState, useEffect, useLayoutEffect as _useLayoutEffect } from 'react';
-
-const useLayoutEffect = typeof window !== 'undefined' ? _useLayoutEffect : useEffect;
+import { useMatchMedia } from '../../match-media';
 
 export const useSystemTheme = ({
   defaultTheme = 'light',
 }: { defaultTheme?: 'light' | 'dark' } = {}) => {
-  const [theme, setTheme] = useState(defaultTheme);
+  const perfersDark = useMatchMedia({
+    query: '(prefers-color-scheme: dark)',
+    defaultValue: defaultTheme === 'dark',
+  });
 
-  useEffect(() => {
-    if (typeof window === 'undefined' || !window.matchMedia) {
-      return;
-    }
-
-    const media = window.matchMedia('(prefers-color-scheme: dark)');
-    const listener = ({ matches }: MediaQueryListEventMap['change']) => {
-      setTheme(matches ? 'dark' : 'light');
-    };
-
-    if (media.addEventListener) {
-      media.addEventListener('change', listener);
-      return () => {
-        media.removeEventListener('change', listener);
-      };
-    }
-
-    media.addListener(listener);
-    return () => {
-      media.removeListener(listener);
-    };
-  }, []);
-
-  useLayoutEffect(() => {
-    if (typeof window === 'undefined' || !window.matchMedia) {
-      return;
-    }
-
-    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      setTheme('dark');
-    } else {
-      setTheme('light');
-    }
-  }, []);
-
-  return theme;
+  return perfersDark ? 'dark' : 'light';
 };
