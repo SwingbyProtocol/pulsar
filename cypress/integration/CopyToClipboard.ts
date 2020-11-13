d7ae66b4d6cd93874dfec7041528889126471d88
Rubén <ruben@swingby.network>
2020-11-13 11:45:26 +0100

    test: Copy to clipboard functionality

--- cypress/integration/CopyToClipboard.ts
+++ cypress/integration/CopyToClipboard.ts
@@ -1,12 +1,23 @@
 describe('CopyToClipboard', () => {
   it('renders correctly', () => {
-    cy.visitStory({ storyId: 'copytoclipboard--default' });
-    cy.percySnapshot('CopyToClipboard');
-  });
+    let clipboard = '';
+    cy.visitStory(
+      { storyId: 'copytoclipboard--default' },
+      {
+        onBeforeLoad: (win) => {
+          win.navigator.clipboard.writeText = async (value) => {
+            clipboard = value;
+          };
+        },
+      },
+    );
 
-  it('copies value to clipboard', () => {
-    cy.visitStory({ storyId: 'textinput--with-error' });
-    cy.get('[data-testid="light.input-country.native-input"]').focus();
-    cy.percySnapshot('TextInput with error');
+    cy.percySnapshot('CopyToClipboard');
+
+    cy.get('[data-testid="light.copy-country"]')
+      .click()
+      .then(() => {
+        expect(clipboard).to.eq('A country-sized CopyToClipboard component');
+      });
   });
 });
