8dff020dc1d4397ac53549dfe9e10a77649e115c
Rubén Illodo Brea <me@rubenillodo.com>
2020-11-16 10:38:43 +0100

    fix: Crash using `formatFiatAsset()` on Safari

--- src/modules/intl/getFiatAssetFormatter/index.tsx
+++ src/modules/intl/getFiatAssetFormatter/index.tsx
@@ -5,11 +5,22 @@ export const getFiatAssetFormatter = ({
   locale: string;
   currency: string;
 }) => {
-  return new Intl.NumberFormat(locale, {
-    style: 'currency',
-    currency,
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-    currencyDisplay: 'narrowSymbol',
-  });
+  try {
+    // If `narrowSymbol` is not supported (e.g. on Safari), this will throw an error.
+    return new Intl.NumberFormat(locale, {
+      style: 'currency',
+      currency,
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+      currencyDisplay: 'narrowSymbol',
+    });
+  } catch (e) {
+    return new Intl.NumberFormat(locale, {
+      style: 'currency',
+      currency,
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+      currencyDisplay: 'symbol',
+    });
+  }
 };
