bdb6e38ce828b7ea28d4816f836633670ddfb38f
Rubén Illodo Brea <me@rubenillodo.com>
2020-11-17 09:32:46 +0100

    feat: Configurable fraction digits in formatter functions

--- src/modules/intl/getFiatAssetFormatter/index.tsx
+++ src/modules/intl/getFiatAssetFormatter/index.tsx
@@ -1,25 +1,29 @@
 export const getFiatAssetFormatter = ({
   locale,
   currency,
+  minimumFractionDigits = 2,
+  maximumFractionDigits = 2,
 }: {
   locale: string;
   currency: string;
+  minimumFractionDigits?: number;
+  maximumFractionDigits?: number;
 }) => {
   try {
     // If `narrowSymbol` is not supported (e.g. on Safari), this will throw an error.
     return new Intl.NumberFormat(locale, {
       style: 'currency',
       currency,
-      minimumFractionDigits: 2,
-      maximumFractionDigits: 2,
+      minimumFractionDigits,
+      maximumFractionDigits,
       currencyDisplay: 'narrowSymbol',
     });
   } catch (e) {
     return new Intl.NumberFormat(locale, {
       style: 'currency',
       currency,
-      minimumFractionDigits: 2,
-      maximumFractionDigits: 2,
+      minimumFractionDigits,
+      maximumFractionDigits,
       currencyDisplay: 'symbol',
     });
   }
--- src/modules/intl/index.tsx
+++ src/modules/intl/index.tsx
@@ -1,4 +1,2 @@
-export { formatCryptoAsset } from './formatCryptoAsset';
-export { formatFiatAsset } from './formatFiatAsset';
 export { getCryptoAssetFormatter } from './getCryptoAssetFormatter';
 export { getFiatAssetFormatter } from './getFiatAssetFormatter';
