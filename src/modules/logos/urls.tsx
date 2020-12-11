2fdcdd0f296f985f0dbff8298ab6e46e1214a67e
tarrio <73276750+tarrio@users.noreply.github.com>
2020-12-11 11:43:14 +0100

    feat: BTCE and WBTC use the same logo (#46)

--- src/modules/logos/urls.tsx
+++ src/modules/logos/urls.tsx
@@ -3,8 +3,6 @@
 export { default as CoinBnb } from './assets/CoinBnb.svg';
 export { default as CoinBtc } from './assets/CoinBtc.svg';
 export { default as CoinBtcb } from './assets/CoinBtcb.svg';
-export { default as CoinBtce } from './assets/CoinBtce.svg';
-export { default as CoinRenbtc } from './assets/CoinRenbtc.svg';
 export { default as CoinWbtc } from './assets/CoinWbtc.svg';
 export { default as StarsBg } from './assets/StarsBg.svg';
 export { default as Swingby } from './assets/Swingby.svg';
