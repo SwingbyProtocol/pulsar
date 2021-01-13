2c053a47ad844e9426e577c1d2f4ddf03b5db7c7
Rubén <ruben@swingby.network>
2021-01-13 16:43:27 +0100

    feat: Added support for `toastId`

--- src/modules/toasts/createToast/index.tsx
+++ src/modules/toasts/createToast/index.tsx
@@ -1,4 +1,4 @@
-import { toast, ToastContent } from 'react-toastify';
+import { toast, ToastContent, ToastOptions } from 'react-toastify';
 
 const TYPES = ['info', 'success', 'warning', 'danger', 'default'] as const;
 type Type = typeof TYPES[number];
@@ -7,16 +7,19 @@ export const createToast = ({
   content,
   autoClose = false,
   type = 'default',
+  toastId,
 }: {
   content: ToastContent;
   autoClose?: boolean | number;
   type?: Type;
+  toastId?: ToastOptions['toastId'];
 }) => {
   toast(content, {
     autoClose: autoClose === true ? 5000 : autoClose,
     hideProgressBar: true,
     position: 'top-center',
     closeOnClick: true,
+    toastId,
     type: (() => {
       switch (type) {
         case 'info':
