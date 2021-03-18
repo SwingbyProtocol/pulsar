644c2cbab5aa5061d24de0ca60401b7d9b7c0c52
Rubén <ruben@swingby.network>
2021-03-18 13:32:49 +0100

    feat: Added support for new symbols to `getCoinLogo()`

--- src/modules/logos/getCoinLogo.tsx
+++ src/modules/logos/getCoinLogo.tsx
@@ -7,13 +7,18 @@ export const getCoinLogo = ({ symbol }: { symbol: string }) => {
     case 'BTCB':
     case 'BTC.B':
     case 'BTCB-1DE':
+    case 'BTCB.BEP20':
       return logos.CoinBtcb;
     case 'BNB':
+    case 'BNB.BEP20':
       return logos.CoinBnb;
     case 'BTCE':
     case 'WBTC':
+    case 'WBTC.ERC20':
       return logos.CoinWbtc;
     case 'sbBTC':
+    case 'sbBTC.ERC20':
+    case 'sbBTC.BEP20':
       return logos.CoinSbbtc;
     default:
       return undefined;
