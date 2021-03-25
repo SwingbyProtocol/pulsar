fd28229c37d02fd14fb2e507db45a52496ec9c61
Rubén <ruben@swingby.network>
2021-03-25 12:07:36 +0100

    feat: `getCoinLogo()` supports `__ERC20`-type suffixes

--- src/modules/logos/getCoinLogo.tsx
+++ src/modules/logos/getCoinLogo.tsx
@@ -8,17 +8,22 @@ export const getCoinLogo = ({ symbol }: { symbol: string }) => {
     case 'BTC.B':
     case 'BTCB-1DE':
     case 'BTCB.BEP20':
+    case 'BTCB__BEP20':
       return logos.CoinBtcb;
     case 'BNB':
     case 'BNB.BEP20':
+    case 'BNB__BEP20':
       return logos.CoinBnb;
     case 'BTCE':
     case 'WBTC':
     case 'WBTC.ERC20':
+    case 'WBTC__ERC20':
       return logos.CoinWbtc;
     case 'sbBTC':
     case 'sbBTC.ERC20':
     case 'sbBTC.BEP20':
+    case 'sbBTC__ERC20':
+    case 'sbBTC__BEP20':
       return logos.CoinSbbtc;
     default:
       return undefined;
