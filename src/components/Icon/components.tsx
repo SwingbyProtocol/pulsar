c592c056b69fe76462f938d59ce3c294a5303297
Rubén <ruben@swingby.network>
2021-04-12 17:32:42 +0200

    feat: Added Binance Chain Wallet icon

--- src/components/Icon/components.tsx
+++ src/components/Icon/components.tsx
@@ -4,6 +4,7 @@ export { ReactComponent as ArrowDown } from './assets/ArrowDown.svg';
 export { ReactComponent as ArrowLeft } from './assets/ArrowLeft.svg';
 export { ReactComponent as ArrowRight } from './assets/ArrowRight.svg';
 export { ReactComponent as ArrowUp } from './assets/ArrowUp.svg';
+export { ReactComponent as BinanceChainWallet } from './assets/BinanceChainWallet.svg';
 export { ReactComponent as CaretDown } from './assets/CaretDown.svg';
 export { ReactComponent as CaretLeft } from './assets/CaretLeft.svg';
 export { ReactComponent as CaretRight } from './assets/CaretRight.svg';
